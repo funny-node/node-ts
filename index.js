4f3edc16feea329d8088b4e64ca6d923920b294a
lessfish <lessfish@outlook.com>
2020-01-03 12:28:00 +0800

    ts 写 node

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+var http = require("http");
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("Hello World!");
+    response.end();
+}).listen(8000);
---
+++ index.ts
@@ -0,0 +1,7 @@
+const http = require("http")
+
+http.createServer((request:any, response:any) => {
+  response.writeHead(200, {"Content-Type": "text/html"})
+  response.write("Hello World!")
+  response.end()
+}).listen(8000)
